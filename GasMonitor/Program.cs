87332247e9ac725649553c89d39190269bee77bc
Kanwaljit <cavey.waveyy@gmail.com>
2020-03-31 11:23:07 +0100

    console log objects from program.cs

--- GasMonitor/Program.cs
+++ GasMonitor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Amazon.S3;
@@ -10,10 +11,12 @@ namespace GasMonitor
     {
         static void Main(string[] args)
         {
-            var task = AmazonService.ReadObjectDataAsync();
-            task.Wait();
-            Console.WriteLine("data {0}", task.Result);
-    
+            var list = AmazonService.ReadObjectDataAsync().Result;
+            
+            foreach (var result in list)
+            {
+                Console.WriteLine($"id:{result.Id}, x:{result.X}, y:{result.Y}");
+            }
         }
     }
 }    
